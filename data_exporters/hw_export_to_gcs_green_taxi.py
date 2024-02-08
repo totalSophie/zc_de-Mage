1a0f1d47fc105ca089450e4425e710867f6bcc9f
Sophie C <totalsophie@gmail.com>
2024-02-08 08:28:26 +0300

    bugfix

--- data_exporters/hw_export_to_gcs_green_taxi.py
+++ data_exporters/hw_export_to_gcs_green_taxi.py
@@ -5,7 +5,7 @@ import os
 if 'data_exporter' not in globals():
     from mage_ai.data_preparation.decorators import data_exporter
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/src/vibestream-410919-b2d4e6f408ae.json"
+
 
 bucket_name = 'data_zc'
 project_id = "vibestream-410919"
